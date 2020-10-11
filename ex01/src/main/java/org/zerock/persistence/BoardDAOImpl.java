0a384142acd115630f0a9a332f72c4d9caaff0a3
taesung <taesung@TAESUNG>
2020-10-11 14:24:59 +0900

    8.페이징 처리 - 영속(persistence)계층, 비즈니스 계층

--- ex01/src/main/java/org/zerock/persistence/BoardDAOImpl.java
+++ ex01/src/main/java/org/zerock/persistence/BoardDAOImpl.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 
 @Repository
 public class BoardDAOImpl implements BoardDAO{
@@ -43,6 +44,25 @@ public class BoardDAOImpl implements BoardDAO{
 	@Override
 	public List<BoardVO> listAll() throws Exception {
 		return session.selectList(namespace+".listAll");
+	}
+
+	@Override
+	public List<BoardVO> listPage(int page) throws Exception {
+		
+		if(page <= 0) {
+			page = 1;
+		}
+		
+		page = (page - 1) * 10;
+		//한 페이지에 10개의 글을 보여준다
+		
+		return session.selectList(namespace + ".listPage", page);
+	}
+
+	@Override
+	public List<BoardVO> listCriteria(Criteria cri) throws Exception {
+		
+		return session.selectList(namespace+ ".listCriteria", cri);
 	}
 	
 	
--- ex01/src/test/java/org/zerock/BoardDAOTest.java
+++ ex01/src/test/java/org/zerock/BoardDAOTest.java
@@ -1,5 +1,7 @@
 package org.zerock;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.junit.Test;
@@ -9,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 import org.zerock.persistence.BoardDAO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -48,4 +51,32 @@ public class BoardDAOTest {
 		dao.delete(1);
 	}
 	
+	@Test
+	public void testListPage()throws Exception{
+		
+		int page = 3;
+		
+		List<BoardVO> list = dao.listPage(page);
+		
+		for (BoardVO boardVO : list) {
+			logger.info(boardVO.getBno() + ":" + boardVO.getTitle());
+		}
+	}
+	
+	@Test
+	public void testListCriteria() throws Exception{
+		/*
+		 * Criteria TEST sql
+		 * select * from tbl_board where bno > 0  order by bno desc limit 20,20;
+		 * */
+		Criteria cri = new Criteria();
+		cri.setPage(2);
+		cri.setPerPageNum(20);
+		
+		List<BoardVO> list = dao.listCriteria(cri);
+		
+		for(BoardVO boardVO : list) {
+			logger.info(boardVO.getBno() + ":" + boardVO.getTitle());
+		}
+	}
 }
